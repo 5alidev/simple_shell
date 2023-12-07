86258ab43d61f0c860c3aa6817d2ea5cd93bf333
5alidev <khalidbenhamou.dev@outlook.com>
2023-12-07 04:58:57 -0800

    Simple shell 0.1

--- main.c
+++ main.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
 int main()
 {
 	char *ptLine = NULL;
-	size_t len;
-	ssize_t checkInput;
+	size_t len = 0;
+	int nbr_chars;
 	char *command = NULL;
+	int i = 0, status;
+	pid_t pid;
+	char **array = NULL;
+	extern char **environ;
 
-	printf(">>");
-	checkInput = getline(&ptLine, &len, stdin);
-	command = strtok(ptLine," \t\n");
-
-	while (command != NULL)
+	while (1)
 	{
-		printf("%s\n", command);
-		command = strtok(NULL, " \t\n");
+		array = malloc(sizeof(char *) * 1024);
+		write(1, "#cisfun$ ",9); /* write 9 chars*/
+		nbr_chars = getline(&ptLine, &len, stdin);
+		command = strtok(ptLine," \t\n");
+		while(command)
+		{
+			array[i] = command;
+			command = strtok(NULL, " \t\n");
+			i++;
+		}
+		array[i] = NULL;
+		pid = fork();
+		if (pid == 0)
+		{
+			if (execve(array[0], array, environ) == -1)
+				printf("No such file or directory\n");
+		}
+		else
+			wait(&status);
+		i = 0;
+		free(array);
+
 	}
 	return (0);
 }
