d1f65202d779f7e795aa827045a274f3cb831e4a
Tisolaso <spanya2017@gmail.com>
2023-12-13 16:00:32 -0800

    lol

--- strings.c
+++ strings.c
@@ -87,3 +87,15 @@ char *_strcat(char *dest, char *src)
 	*ptr = *src;
 	return (dest);
 }
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
